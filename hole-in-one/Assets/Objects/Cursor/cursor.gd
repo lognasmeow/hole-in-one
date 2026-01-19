80d1a471fe0ca1e273c78cc3724b5326d0d4a387
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-01-19 13:39:44 -0500

    hide mouse

--- hole-in-one/Assets/Objects/Cursor/cursor.gd
+++ hole-in-one/Assets/Objects/Cursor/cursor.gd
@@ -4,6 +4,9 @@ const grabHoverPng = preload("res://Assets/UI/Cursors/grab_hover.png")
 const grabPng = preload("res://Assets/UI/Cursors/grab.png")
 const linkPng = preload("res://Assets/UI/Cursors/link.png")
 
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position = get_global_mouse_position()
