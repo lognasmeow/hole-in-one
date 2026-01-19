5f9f27270758ff4f25ca5c5567fec12be95bc3cf
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-01-19 13:36:11 -0500

    Change cursor to built in mouse

--- hole-in-one/Assets/Objects/Cursor/cursor.gd
+++ hole-in-one/Assets/Objects/Cursor/cursor.gd
@@ -1,12 +1,17 @@
-extends Sprite3D
+extends Node2D
 
 const grabHoverPng = preload("res://Assets/UI/Cursors/grab_hover.png")
 const grabPng = preload("res://Assets/UI/Cursors/grab.png")
 const linkPng = preload("res://Assets/UI/Cursors/link.png")
 
+func _ready():
+	Input.set_custom_mouse_cursor(grabHoverPng)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	position = get_global_mouse_position()
+	
 	if Input.is_action_pressed("click"):
-		texture = grabPng
+		Input.set_custom_mouse_cursor(grabPng)
 	elif Input.is_action_just_released("click"):
-		texture = grabHoverPng
+		Input.set_custom_mouse_cursor(grabHoverPng)
