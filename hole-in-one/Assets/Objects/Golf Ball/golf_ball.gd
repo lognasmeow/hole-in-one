7efa27bab95795165353d06cd8e3c6edaa15b1f9
Lognas <42080354+lognasmeow@users.noreply.github.com>
2026-01-19 17:39:16 -0500

    this sucks

--- hole-in-one/Assets/Objects/Golf Ball/golf_ball.gd
+++ hole-in-one/Assets/Objects/Golf Ball/golf_ball.gd
@@ -1,13 +1,20 @@
-extends Sprite2D
+extends CharacterBody2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	
-func _process(delta):
-	position.x += 1
-	position.y += 1
+func _physics_process(delta):
+	var speed: float = 1
+	var velocity = Vector2(1, -1)
+	var collision = move_and_collide(velocity * speed)
+	
+	if collision:
+		print("collision")
+		velocity = velocity.bounce(collision.get_normal())
+		#velocity *= 0.9
+		#move_and_collide(collision.get_remainder())
 
 
 func hit(direction: Vector2, speed: float):
